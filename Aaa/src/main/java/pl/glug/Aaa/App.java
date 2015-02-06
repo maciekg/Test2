d206587844f848de4cc9c391f279c05d87b8be6f
Maciej <Maciej@192.168.0.10>
2015-02-06 17:55:43 +0100

    first

---
+++ Aaa/src/main/java/pl/glug/Aaa/App.java
@@ -0,0 +1,13 @@
+package pl.glug.Aaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
